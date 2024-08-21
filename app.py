e10ce4b67b256bf50589c4a886edf0027ddc8d88
m1016m <m1016m@gmail.com>
2024-08-21 18:36:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ def handle_message(event):
             line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
     ######################## 使用說明 選單 油價查詢################################
-    if event.message.text == "油價報你知":
+    if event.message.text == "油價查詢":
         content = oil_price()
         line_bot_api.reply_message(
             event.reply_token,
