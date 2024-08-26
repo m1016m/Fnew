b4a3a9b093a1d5600be99af8d344012336ba1190
m1016m <m1016m@gmail.com>
2024-08-26 16:02:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ import stockprice
 import requests, json, time
 import place
 import pandas as pd
-from keras.models import load_model
-from PIL import Image
+# from tensorflow.keras.models import load_model
+# from PIL import Image
 import numpy as np
 import io
 import os
@@ -33,17 +33,7 @@ access_token = 'jIdH9Ta/KYSrc3bfb8HpD5aG9FpRyLU+b0uZZ9/q8ckCjSF0gEaqBd0dlNN375Wo
 channel_secret = '827fe25e726242685799d486978af9cc'
 #暫存用dict
 mat_d={}
-# 加載你的CNN模型
-model = load_model('mnist_cnn_model.h5')
 
-def preprocess_image(image):
-    image = image.convert('L')
-    image = image.resize((28, 28))
-    image = np.array(image)
-    image = image / 255.0
-    image = np.expand_dims(image, axis=0)
-    image = np.expand_dims(image, axis=-1)
-    return image
 # 這段主要在畫K線圖
 # pip3 install pyimgur
 import yfinance as yf
@@ -562,23 +552,7 @@ def handle_message(event):
         while True: 
             schedule.run_pending()
             time.sleep(1)
-    if re.match('CNN|cnn',msg):
-        # 獲取圖片內容
-        message_content = line_bot_api.get_message_content(event.message.id)
-        image = Image.open(io.BytesIO(message_content.content))
-
-        # 預處理圖片
-        image = preprocess_image(image)
-
-        # 執行CNN模型進行預測
-        prediction = model.predict(image)
-        digit = np.argmax(prediction)
-
-        # 回傳預測結果
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f'預測的數字是: {digit}')
-        )
+    
     #＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊weather＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊#
     # 圖文選單
     # 第一層-最新氣象->4格圖片Flex Message
